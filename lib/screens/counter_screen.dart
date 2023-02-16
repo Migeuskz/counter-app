10050a0fdff392f331eab12e7d752fe32dad3d78
Migeuskz <msanchezpapalotzi@gmail.com>
2023-02-16 11:58:40 -0600

    Terminando Proyecto

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -9,6 +9,22 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void reiniciar() {
+    contador = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const TextStyle estiloTexto = TextStyle(
@@ -38,29 +54,51 @@ class _CounterScreenState extends State<CounterScreen> {
         ],
       )),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            backgroundColor: const Color.fromARGB(255, 255, 204, 0),
-            foregroundColor: Colors.black,
-            onPressed: () => setState(() => contador--),
-            child: const Icon(Icons.exposure_minus_1),
-          ),
-          FloatingActionButton(
-            backgroundColor: const Color.fromARGB(255, 255, 204, 0),
-            foregroundColor: Colors.black,
-            onPressed: () => setState(() => contador = 0),
-            child: const Icon(Icons.restart_alt),
-          ),
-          FloatingActionButton(
-            backgroundColor: const Color.fromARGB(255, 255, 204, 0),
-            foregroundColor: Colors.black,
-            onPressed: () => setState(() => contador++),
-            child: const Icon(Icons.exposure_plus_1),
-          ),
-        ],
+      floatingActionButton: CustomFloatingActionBotton(
+        decrementar: decrementar,
+        incrementar: incrementar,
+        reiniciar: reiniciar,
       ),
+    );
+  }
+}
+
+class CustomFloatingActionBotton extends StatelessWidget {
+  final Function incrementar;
+  final Function decrementar;
+  final Function reiniciar;
+
+  const CustomFloatingActionBotton({
+    super.key,
+    required this.incrementar,
+    required this.decrementar,
+    required this.reiniciar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 255, 204, 0),
+          foregroundColor: Colors.black,
+          onPressed: () => decrementar(),
+          child: const Icon(Icons.exposure_minus_1),
+        ),
+        FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 255, 204, 0),
+          foregroundColor: Colors.black,
+          onPressed: () => reiniciar(),
+          child: const Icon(Icons.restart_alt),
+        ),
+        FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 255, 204, 0),
+          foregroundColor: Colors.black,
+          onPressed: () => incrementar(),
+          child: const Icon(Icons.exposure_plus_1),
+        ),
+      ],
     );
   }
 }
